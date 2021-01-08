b8869254aeaa0c7a1e0fd2e4a70790cad2541c2d
Raul Arellano Guevara <raularellanoguevara@gmail.com>
2021-01-08 17:36:43 -0600

    make it better

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname+"/public"));
+
+
+app.get('/', (req, res) => {
+    res.render("index",{
+        titulo:"titulo de forma dinámica index"
+    })
+})
+
+app.get('/servicio', (req, res) => {
+    res.render('servicio',{
+        tituloServicios:"Pagina de servicios"
+    })
+})
+
+app.use((req,res,next)=>{
+    res.status(404).render("404",{
+        titulo404:"Página 404",
+        descripcion:"este es un error"
+    })
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port port!`)
+})
+
+
